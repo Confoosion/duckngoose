33ffc182c8f6e963053b7b07a2aadffed34112cd
Calvin Hunter <calvinhunter03@gmail.com>
2025-04-08 18:16:09 -0700

    the phone now works, you can press F and it appears, thats all for now follks

--- Assets/CanvasScript.cs
+++ Assets/CanvasScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class CanvasScript : MonoBehaviour
 {
+    [SerializeField] public GameObject phoneUI;
+    private bool enabled = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (enabled)
+            {
+                phoneUI.SetActive(false);
+                enabled = !enabled;
+            }
+            else
+            {
+                phoneUI.SetActive(true);
+                enabled = !enabled;
+
+            }
+        }
     }
 }
